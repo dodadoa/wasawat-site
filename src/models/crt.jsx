0df5086b7a8bf190ab4641bd4f2614e96ab21bd7
dodadoa <maddodadoa@gmail.com>
2023-01-17 11:29:20 +0700

    [feat] styling more scene

--- src/models/crt.jsx
+++ src/models/crt.jsx
@@ -1,26 +1,28 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { useSpring, animated } from '@react-spring/three'
+
 
 export function Model(props) {
   const { nodes, materials } = useGLTF('/models/crt/scene-transformed.glb')
   const ref = useRef()
   const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-
-  const { position } = useSpring({ 
-    position: clicked ? [-2, 0, 0]: [0, 0, 0]
-  })
 
   useFrame((state, delta) => {
     ref.current.rotation.y -= 0.005
   })
 
   return (
-    <animated.group {...props} position={position} dispose={null} ref={ref} onClick={() => click(!clicked)}>
-      <group scale={0.05}>
-        <group position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={10}>
+    <animated.group 
+      onPointerEnter={props.handlePointerEnterCrt} 
+      onPointerLeave={props.handlePointerLeaveCrt} 
+      dispose={null} 
+      ref={ref} 
+      onClick={props.handleClickCrt}
+    >
+      <group scale={0.03}>
+        <group position={[0, 10, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={10}>
           <mesh geometry={nodes.CRT_Monitor_monitor_glass_0.geometry} material={materials.monitor_glass} />
           <mesh geometry={nodes.CRT_Monitor_monitor_plastic_0.geometry} material={materials.monitor_plastic} />
         </group>
--- src/subpages/index/threejs/Scene.jsx
+++ src/subpages/index/threejs/Scene.jsx
@@ -1,13 +1,28 @@
 import React, { Fragment, useRef, useState, forwardRef } from "react"
-import { useFrame } from "@react-three/fiber"
 import { Model } from "../../../models/crt"
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
 
-export default function Scene(props) {
+export default function Scene() {
+  const handleClickCrt = () => {
+    console.log('click')
+  }
+
+  const [crtHover, setCrtHover] = useState(false)
+  const handlePointerEnterCrt = () => setCrtHover(true)
+  const handlePointerLeaveCrt = () => setCrtHover(false)
+
   return (
-    <Fragment>
+    <group>
+      <EffectComposer>
+        {crtHover && <Bloom mipmapBlur luminanceThreshold={0} luminanceSmoothing={1.3} />}
+      </EffectComposer>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Model />
-    </Fragment>
+      <Model
+        handleClickCrt={handleClickCrt} 
+        handlePointerEnterCrt={handlePointerEnterCrt}
+        handlePointerLeaveCrt={handlePointerLeaveCrt}
+      />
+    </group>
   )
 }
